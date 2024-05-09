465e1ce69b96e618a5970a5c931dafca303a8801
brian110326 <brian110326@naver.com>
2024-05-09 13:15:15 +0900

    python

--- ch1/operator.py
+++ ch1/operator.py
@@ -3,6 +3,17 @@ a,b = 5,3
 print(a+b)
 print(a-b)
 print(a*b)
-print(a/b)
-print(a//b)
-print(a%b)
+print(a/b) #1.6666666666666667
+print(a//b) #몫 계산
+print(a%b) #나머지
+print(a**b) #거듭제곱
+
+#대입연산자
+a += 5
+print(a)
+
+a *= 5
+print(a)
+
+a -= 5
+print(a)
