16be34f77f3970c75adaf20d1138b8f8edd0340b
brian110326 <brian110326@naver.com>
2024-05-10 12:49:13 +0900

    반복문

--- ch1/loop.py
+++ ch1/loop.py
@@ -76,3 +76,42 @@ num = int(input("숫자 입력 : "))
 print(sum(range(1, num + 1)))
 
 # %%
+for s in "dreams":
+    print(s, end="")
+
+# %%
+for i in range(3):
+    for j in range(3):
+        print(j, end=" ")
+    print()
+
+# %%
+for i in range(2, 10):
+    for j in range(1, 10):
+        print("%d X %d = %2d" % (i, j, i * j), end="\t")
+    print()
+
+# %%
+# list ==> 배열
+numbers = [14, 3, 4, 5, 1, 77, 88, 46, 34]
+for number in numbers:
+    print(number)
+
+# %%
+# break
+i = 1
+while i < 11:
+    if i == 5:
+        break
+    print(i, end=" ")
+    i += 1
+
+# %%
+i = 1
+while i < 11:
+    if i % 2 == 0:
+        continue
+    print(i, end=" ")
+    i += 1
+
+# %%
