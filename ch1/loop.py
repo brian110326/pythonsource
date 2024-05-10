edada55b7c5f6981f16a7de0d8ec773e6d4c4f60
brian110326 <brian110326@naver.com>
2024-05-10 11:50:47 +0900

    반복문

--- ch1/loop.py
+++ ch1/loop.py
@@ -1,2 +1,25 @@
 # %%
 # while / for문
+i = 1
+while i < 11:
+    print(i)
+    i += 1
+    # i++ 안됨
+
+# %%
+i = 1
+while i < 101:
+    if i % 2 == 0:
+        print(i, end=" ")
+    i += 1
+
+# %%
+i = 1
+sum1 = 0
+while i < 101:
+    sum1 += i
+    i += 1
+print(sum1)
+# sum함수가 있어서 변수명은 sum X
+
+# %%
