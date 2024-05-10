fb2e3b0bef8ac5828b8e15ae19fdf384def74234
brian110326 <brian110326@naver.com>
2024-05-10 12:18:27 +0900

    반복문

--- ch1/loop.py
+++ ch1/loop.py
@@ -23,3 +23,56 @@ print(sum1)
 # sum함수가 있어서 변수명은 sum X
 
 # %%
+i = 1
+while i < 10:
+    print("6 X %d = %2d" % (i, 6 * i))
+    i += 1
+
+# %%
+while True:
+    str = input("입력 : ")
+    if str == "q":
+        break
+    print(str)
+
+
+# %%
+print(list(range(5)))
+print(list(range(1, 5, 2)))
+
+# %%
+i = 10
+i + 1
+
+# %%
+for i in range(11):
+    print(i)
+
+# %%
+for i in range(1, 11):
+    print(i)
+
+# %%
+for i in range(1, 101, 2):
+    print(i, end=" ")
+
+# %%
+sum1 = 0
+for i in range(1, 101):
+    sum1 += i
+print(sum1)
+
+# %%
+# sum()
+print(sum(range(1, 101)))
+
+
+# %%
+range(10, 1)
+print(list(range(10, 1, -1)))
+
+# %%
+num = int(input("숫자 입력 : "))
+print(sum(range(1, num + 1)))
+
+# %%
