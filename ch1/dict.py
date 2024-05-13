252a3f66dfe18654ebcb3e3e5582eab712510046
brian110326 <brian110326@naver.com>
2024-05-13 13:16:52 +0900

    dictionary

--- ch1/dict.py
+++ ch1/dict.py
@@ -7,4 +7,21 @@
 # 생성
 dict1 = {}
 dict2 = {"name": "Song", "age": 12}
-dict2 = {0: "Hello Pyhton", 1: "Hello Coding"}
+dict3 = {0: "Hello Pyhton", 1: "Hello Coding"}
+dict4 = {"arr": [1, 2, 3, 4, 5]}
+
+# %%
+print(dict2)
+print(dict3)
+print(dict4)
+
+# %%
+# 특정 key의 요소 출력
+print(dict2["name"])
+print(dict3[0])
+print(dict4["arr"])
+
+# %%
+# dict1["addr"] : KeyError
+
+# %%
