05d8d6955e1860a186004f07edfacd62b1cf62f2
brian110326 <brian110326@naver.com>
2024-05-28 11:20:24 +0900

    crawl

---
+++ crawl/google_news.py
@@ -0,0 +1,32 @@
+# 파이썬 뉴스기사 크롤링
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://news.google.com/search?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&hl=ko&gl=KR&ceid=KR%3Ako"
+base_url = "https://news.google.com"
+
+with requests.Session() as s:
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    articles = soup.select("div.UW0SDc article")
+    for article in articles:
+        aTags = article.select("a:nth-child(2)")
+        for aTag in aTags:
+            title = aTag.string
+            href = base_url + aTag["href"][1:]
+            # print(f"제목 : {title}, href : {href}")
+
+        report_date_time = article.select_one("time")
+
+        if report_date_time:
+            report_date_time = report_date_time["datetime"].split("T")
+            report_date = report_date_time[0]
+            report_time = report_date_time[1]
+        else:
+            report_date = ""
+            report_time = ""
+
+    comps = soup.find_all("div", class_="vr1PYe")
+    for comp in comps:
+        print(comp.string)
