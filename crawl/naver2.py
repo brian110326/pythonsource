35eebda967705554db360869f52e5ea951d04af0
brian110326 <brian110326@naver.com>
2024-05-28 13:20:40 +0900

    naver open api

--- crawl/naver2.py
+++ crawl/naver2.py
@@ -1,21 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.error import HTTPError
+from openpyxl import Workbook
+from xlsx_write import write_excel_template
 
-url = "https://openapi.naver.com/v1/search/shop.json"
+wb = Workbook()
+ws = wb.active
+ws.title = "Naver Open API"
+ws.column_dimensions["B"].width = 100
+ws.column_dimensions["C"].width = 60
+
+ws.append(["순위", "상품명", "판매경로"])
+
 headers = {
     "X-Naver-Client-Id": "QAkT4398UqNmY3pM9Xaz",
     "X-Naver-Client-Secret": "MZ6XvCh9Q1",
 }
-params = {"query": "yeezy"}
 
-r = requests.get(url, headers=headers, params=params)
+start, num = 1, 0
+for idx in range(10):
+    # idx : 0 ~ 9
+    start_num = start + (idx * 100)
+    url = "https://openapi.naver.com/v1/search/shop.json"
+    params = {"query": "iphone", "display": 100, "start": str(start_num)}
+    r = requests.get(url, headers=headers, params=params)
 
-# json 가져오기
-data = r.json()
-# print(data)
+    # print(r.url)
 
-# print(data["items"])
+    # json 가져오기
+    data = r.json()
+    # print(data)
 
-for idx, item in enumerate(data["items"], 1):
-    print(idx, item["title"], item["link"])
+    # print(data["items"])
+
+    for idx, item in enumerate(data["items"], 1):
+        # print(idx, item["title"], item["link"])  # <b>아이폰</b>
+        ws.append([num, item["title"], item["link"]])
+        num += 1
+
+base_dir = "./crawl/file/"
+wb.save(base_dir + "naver.xlsx")
+wb.close()
