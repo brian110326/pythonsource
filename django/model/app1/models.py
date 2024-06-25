491885656059d372e6606fb598456c4c72c7578c
brian <brian110326@gmail.com>
2024-06-25 17:59:10 +0900

    django model

--- django/model/app1/models.py
+++ django/model/app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "person"
+
+    # makemigrations 대상 X
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
