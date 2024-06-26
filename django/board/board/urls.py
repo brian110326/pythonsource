c295e7f4b6c36ada01766126099634c6c9ca0985
brian <brian110326@gmail.com>
2024-06-26 12:37:23 +0900

    django board

--- django/board/board/urls.py
+++ django/board/board/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "board"
 
 urlpatterns = [
+    # http://127.0.0.1:8000/board/
     path("", views.question_list, name="question_list"),
+    # http://127.0.0.1:8000/board/1
+    path("<int:qid>/", views.question_detail, name="question_detail"),
 ]
--- django/board/board/views.py
+++ django/board/board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question, Answer
 
 # Create your views here.
@@ -6,6 +6,13 @@ from .models import Question, Answer
 
 def question_list(request):
     # 전체 질문 추출
-    question_list = Question.objects.all()
-    context = {"question_list", question_list}
+    # question_list = Question.objects.all()
+    question_list = Question.objects.order_by("-created_at")
+    context = {"question_list": question_list}
     return render(request, "board/question_list.html", context)
+
+
+def question_detail(reqeust, qid):
+    question = get_object_or_404(Question, id=qid)
+    context = {"question": question}
+    return render(reqeust, "board/question_detail.html", context)
