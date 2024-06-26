8aaf4db76e87c7d5993c23116020be0a1114b1ac
brian <brian110326@gmail.com>
2024-06-26 18:54:32 +0900

    django model

--- django/model/users/urls.py
+++ django/model/users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path, include
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", views.common_login, name="login"),
+]
--- django/model/users/views.py
+++ django/model/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserForm()
+
+    return render(request, "register.html", {"form": form})
+
+
+def common_login(request):
+    pass
