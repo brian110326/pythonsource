3ffbdc0d0e9336a8a783e6308f5596ae923d0b59
brian <brian110326@gmail.com>
2024-06-26 11:07:18 +0900

    django board

--- django/board/board/models.py
+++ django/board/board/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200, verbose_name="제목")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.content
