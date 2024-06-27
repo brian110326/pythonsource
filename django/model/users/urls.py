c50bf8703fc7808e07f28ae1f5b6e3e14771a4e1
brian110326 <brian110326@naver.com>
2024-06-27 10:04:57 +0900

    django model

--- django/model/users/urls.py
+++ django/model/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.common_login, name="login"),
+    path("", views.index, name="index"),
 ]
--- django/model/users/views.py
+++ django/model/users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
 
 
 def register(request):
@@ -16,4 +21,15 @@ def register(request):
 
 
 def common_login(request):
-    pass
+    if request.method == "POST":
+        # 입력값 가져오기
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # db에 들어가서 확인하는 과정
+        user = authenticate(request, username, password)
+
+        if user is not None:
+            login(request, user)
+
+    return render(request, "login.html")
