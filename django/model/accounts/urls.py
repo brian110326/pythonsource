9aa57a53a728b48837569b761fd97a32570de12a
brian <brian110326@gmail.com>
2024-06-27 17:10:56 +0900

    django model

--- django/model/accounts/urls.py
+++ django/model/accounts/urls.py
@@ -40,12 +40,17 @@ urlpatterns = [
     ),
     path(
         "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url=reverse_lazy("common:password_reset_complete"),
+        ),
         name="password_reset_confirm",
     ),
     path(
         "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html",
+        ),
         name="password_reset_complete",
     ),
 ]
