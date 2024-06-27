ccbaa99c23afe3ecc6d469056e8cfaab5839a600
brian <brian110326@gmail.com>
2024-06-27 18:01:13 +0900

    django board

--- django/board/common/urls.py
+++ django/board/common/urls.py
@@ -10,13 +10,19 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     # login 처리를 하는 뷰가 함수형 뷰가 아니라 클래스뷰임
     # 클래스뷰를 함수형처럼 사용하려면 as_view() 사용
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="common/login.html",
+        ),
+        name="login",
+    ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # 기존 비밀번호 변경 후 세션 값 다시 담아줌
     path(
         "password_change/",
         auth_views.PasswordChangeView.as_view(
-            template_name="registration/password_change.html",
+            template_name="common/password_change.html",
             success_url=reverse_lazy("index"),
         ),
         name="password_change",
@@ -25,8 +31,8 @@ urlpatterns = [
     path(
         "password_reset/",
         auth_views.PasswordResetView.as_view(
-            template_name="registration/password_reset.html",
-            email_template_name="registration/password_reset_email.txt",
+            template_name="common/password_reset.html",
+            email_template_name="common/password_reset_email.txt",
             success_url=reverse_lazy("common:password_reset_done"),
         ),
         name="password_reset",
@@ -34,14 +40,14 @@ urlpatterns = [
     path(
         "password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(
-            template_name="registration/password_reset_done.html",
+            template_name="common/password_reset_done.html",
         ),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
-            template_name="registration/password_reset_confirm.html",
+            template_name="common/password_reset_confirm.html",
             success_url=reverse_lazy("common:password_reset_complete"),
         ),
         name="password_reset_confirm",
@@ -49,7 +55,7 @@ urlpatterns = [
     path(
         "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(
-            template_name="registration/password_reset_complete.html",
+            template_name="common/password_reset_complete.html",
         ),
         name="password_reset_complete",
     ),
--- django/board/common/views.py
+++ django/board/common/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = UserForm()
+
+    return render(request, "register.html", {"form": form})
