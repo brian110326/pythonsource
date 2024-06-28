1e72e8b6e920a9ee5c16cb78ba554789e2eff009
brian110326 <brian110326@naver.com>
2024-06-28 16:07:28 +0900

    django board

--- django/board/board/views.py
+++ django/board/board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question, Answer
+from .models import Question, Answer, Comment
 from .forms import QuestionForm, AnswerForm, CommentForm
 
 from django.core.paginator import Paginator
@@ -125,36 +125,87 @@ def answer_delete(request, aid):
 
 @login_required(login_url="common:login")
 def comment_create_question(request, qid):
+    question = get_object_or_404(Question, id=qid)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
-            form.save()
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.question = question
+            comment.save()
+            return redirect("board:question_detail", qid)
     else:
         form = CommentForm()
+
+    return render(
+        request,
+        "board/comment_form.html",
+        {"form": form},
+    )
+
+
+@login_required(login_url="common:login")
+def comment_modify_question(request, cid):
+    comment = get_object_or_404(Comment, id=cid)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.modified_at = timezone.now()
+            comment.save()
+            return redirect("board:question_detail", comment.question.id)
+    else:
+        form = CommentForm(instance=comment)
 
     return render(request, "board/comment_form.html", {"form": form})
 
 
 @login_required(login_url="common:login")
-def comment_modify_question(request, cid):
-    pass
-
-
-@login_required(login_url="common:login")
 def comment_delete_question(request, cid):
-    pass
+    comment = get_object_or_404(Comment, id=cid)
+    comment.delete()
+    return redirect("board:question_detail", comment.question.id)
 
 
 @login_required(login_url="common:login")
 def comment_create_answer(request, aid):
-    pass
+    answer = get_object_or_404(Answer, id=aid)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.answer = answer
+            comment.save()
+            return redirect("board:question_detail", aid)
+    else:
+        form = CommentForm()
+
+    return render(
+        request,
+        "board/comment_form.html",
+        {"form": form},
+    )
 
 
 @login_required(login_url="common:login")
 def comment_modify_answer(request, cid):
-    pass
+    comment = get_object_or_404(Comment, id=cid)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.modified_at = timezone.now()
+            comment.save()
+            return redirect("board:question_detail", comment.answer.question.id)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, "board/comment_form.html", {"form": form})
 
 
 @login_required(login_url="common:login")
 def comment_delete_answer(request, cid):
-    pass
+    comment = get_object_or_404(Comment, id=cid)
+    comment.delete()
+    return redirect("board:question_detail", comment.answer.question.id)
