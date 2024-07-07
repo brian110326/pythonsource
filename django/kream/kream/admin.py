4eda34abdfc6a944f8af7e82cb3467a9922b0ab4
brian <brian110326@gmail.com>
2024-07-07 13:08:56 +0900

    kream project

--- django/kream/kream/admin.py
+++ django/kream/kream/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Trade
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name_kor", "product_detail_url")
+    list_display_links = ["name_kor"]
+    search_fields = ["name_kor"]
+
+
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ("name_kor", "trade_date")
+    list_display_links = ["name_kor"]
+    search_fields = ["name_kor"]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Trade, TradeAdmin)
--- django/kream/kream/models.py
+++ django/kream/kream/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Product(models.Model):
+    brand = models.CharField(max_length=30)
+    name_kor = models.CharField(max_length=30)
+    name_eng = models.CharField(max_length=30)
+    model_no = models.CharField(max_length=20)
+    release_date = models.CharField(max_length=20)
+    color = models.CharField(max_length=10)
+    img_url = models.CharField(max_length=200)
+    product_detail_url = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name_kor
+
+
+class Trade(models.Model):
+    name_kor = models.CharField(max_length=30)
+    trade_date = models.CharField(max_length=30)
+    trade_size = models.CharField(max_length=30)
+    trade_price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name_kor
