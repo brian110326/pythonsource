7fafdd6fc0cbda5523b8711ce890c6707f2d3ef4
brian <brian110326@gmail.com>
2024-07-08 12:01:45 +0900

    kream project

--- django/kream/static/js/detail.js
+++ django/kream/static/js/detail.js
@@ -1,6 +1,9 @@
 const curr_page_year = document.querySelector("#curr_page_year").value;
 const curr_years = document.querySelectorAll("#monthly_sales");
 
+let curr_year_sales = 0;
+let prev_year_sales = 0;
+
 curr_years.forEach((element) => {
   year = element.getAttribute("data-year");
   //console.log(year);
@@ -13,11 +16,13 @@ curr_years.forEach((element) => {
 });
 
 const inc = document.querySelector("#increase");
+const text = document.querySelector("#increaseText");
 
 let calculation;
 
 if (!prev_year_sales) {
   calculation = 0;
+  text.innerHTML = "이전년도 거래내역이 없습니다";
 } else {
   calculation = ((curr_year_sales - prev_year_sales) / prev_year_sales) * 100;
 }
