4dc8891c7cf23224a7ac342a2d0f91d9191b290d
brian <brian110326@gmail.com>
2024-07-09 13:19:25 +0900

    kream project

--- django/kream/common/models.py
+++ django/kream/common/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    tel = models.CharField(max_length=20, null=True, blank=True)
+    address = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
--- django/kream/common/views.py
+++ django/kream/common/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
 
 def register(request):
@@ -16,4 +18,17 @@ def register(request):
 
 
 def profile(request):
+    user = request.user
+
+    if request.method == "POST":
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.tel = request.POST.get("tel")
+        user.address = request.POST.get("address")
+        user.email = request.POST.get("email")
+
+        user.save()
+
+        return redirect("common:profile")
+
     return render(request, "common/profile.html")
